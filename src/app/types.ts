33b8fbadc7c6114486bc98286bace31684226415
Vukan <vukan.markovic97@gmail.com>
2019-07-31 16:45:19 +0200

    initial commit

---
+++ src/app/types.ts
@@ -0,0 +1,15 @@
+export interface Activity {
+    id: string;
+    video_url: string;
+    name: string;
+    description: string;
+    duration: string;
+    created_by: string;
+    image: string;
+    thumbnail: string;
+    cropped: string;
+    file_name_original: string;
+    popularity: string;
+    category_id: string;
+    keywords: string;
+}
